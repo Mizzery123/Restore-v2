b753f4bb8f5fdaa8da6ae2caa9beeccac8463f8c
Bobba <41039452+Mizzery123@users.noreply.github.com>
2025-11-25 13:52:32 +0800

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi(); //Not covered
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. (AKA Middleware - Receive HTTP requests and return responses)
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
+
+//app.UseHttpsRedirection(); // Not used
+
+//app.UseAuthorization(); // Not used
+
+app.MapControllers();
+
+DbInitializer.InitDb(app);
+
+app.Run();
